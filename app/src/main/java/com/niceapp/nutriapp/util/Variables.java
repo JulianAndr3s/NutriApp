163daba971be66340dce0a2f61bc250afe9e9a38
Gil Serna <santiago16gs@gmail.com>
2019-05-31 23:30:58 -0500

    Trae comidas y consejos random desde la base de datos. Genera el IMC, clasifica los pacientes Trae los datos personales de la persona

---
+++ app/src/main/java/com/niceapp/nutriapp/util/Variables.java
@@ -0,0 +1,18 @@
+package com.niceapp.nutriapp.util;
+
+public class Variables {
+
+    public class TipoPaciente {
+        public static final String NIÑO = "Niño";
+        public static final String ADULTO = "Adulto";
+        public static final String ANCIANO = "Anciano";
+    }
+
+    public class EstadoPaciente {
+
+        public static final String DESNUTRICION = "Desnutricion";
+        public static final String NORMAL = "Peso normal";
+        public static final String SOBREPESO = "SOBREPESO";
+
+    }
+}
